1afb43c3706b8146a64168acfb5d7a8b487ffc91
Felipe Hernández <cephei.1313@gmail.com>
2018-05-07 19:34:56 -0300

    added odbyroute and speed urls

--- locustfile.py
+++ locustfile.py
@@ -1,46 +1,105 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from bs4 import BeautifulSoup
 from locust import HttpLocust, TaskSet, task
 
 import urls
-from bs4 import BeautifulSoup
 
 
-class UserBehavior(TaskSet):
+def login(l):
+    login_url = urls.Login()
+    response = l.client.get(login_url.login_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    csrfmiddlewaretoken_value = soup.find('input', {'name': 'csrfmiddlewaretoken'}).get('value')
+    params = login_url.login_parameters.copy()
+    params['csrfmiddlewaretoken'] = csrfmiddlewaretoken_value
 
-    def login(self):
-        login_url = urls.Login()
-        response = self.client.get(login_url.login_url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        csrfmiddlewaretoken_value = soup.find('input', {'name': 'csrfmiddlewaretoken'}).get('value')
-        params = login_url.login_parameters.copy()
-        params['csrfmiddlewaretoken'] = csrfmiddlewaretoken_value
+    l.client.post(login_url.login_url, params)
 
-        self.client.post(login_url.login_url, params)
+
+class ProfileUserBehavior(TaskSet):
+
+    @task(60)
+    def expedition(self):
+        profile_url = urls.Profile()
+        self.client.get(profile_url.expedition_url)
+        self.client.get(profile_url.expedition_data_url, params=profile_url.get_expedition_data_params(),
+                        name=profile_url.expedition_data_url)
+
+    @task(10)
+    def bus_stop(self):
+        profile_url = urls.Profile()
+        self.client.get(profile_url.stop_url)
+        self.client.get(profile_url.stop_data_url, params=profile_url.get_stop_data_params(),
+                        name=profile_url.stop_data_url)
+
+    @task(10)
+    def matrix(self):
+        odbyroute_url = urls.ODByRoute()
+        self.client.get(odbyroute_url.matrix_url)
+        self.client.get(odbyroute_url.matrix_data_url, params=odbyroute_url.get_matrix_data_params(),
+                        name=odbyroute_url.matrix_data_url)
+
+    @task(5)
+    def trajectory(self):
+        profile_url = urls.Profile()
+        self.client.get(profile_url.trajectory_url)
+
+    @task(5)
+    def stop(self):
+        self.interrupt()
+
+
+class SpeedUserBehavior(TaskSet):
+
+    @task(30)
+    def matrix(self):
+        speed_url = urls.Speed()
+        self.client.get(speed_url.matrix_url)
+        self.client.get(speed_url.matrix_data_url, params=speed_url.get_matrix_data_params(),
+                        name=speed_url.matrix_data_url)
+
+    @task(30)
+    def ranking(self):
+        speed_url = urls.Speed()
+        self.client.get(speed_url.ranking_url)
+        self.client.get(speed_url.ranking_data_url, params=speed_url.get_ranking_data_params(),
+                        name=speed_url.ranking_data_url)
+
+    @task(30)
+    def trajectory(self):
+        speed_url = urls.Speed()
+        self.client.get(speed_url.variation_url)
+        self.client.get(speed_url.variation_data_url, params=speed_url.get_variation_data_params(),
+                        name=speed_url.variation_data_url)
+
+    @task(10)
+    def stop(self):
+        self.interrupt()
+
+
+class TripUserBehavior(TaskSet):
+
+    @task
+    def index(self):
+        pass
+
+
+
+class MasterUserBehavior(TaskSet):
+    tasks = {
+        ProfileUserBehavior: 60,
+        SpeedUserBehavior: 20,
+        TripUserBehavior: 20
+    }
 
     def on_start(self):
         """ on_start is called when a Locust start before any task is scheduled """
-        self.login()
-
-    @task(10)
-    def expedition(self):
-        profile_url = urls.Profile()
-        self.client.get(profile_url.expedition)
-        self.client.get(profile_url.expedition_data_url, params=profile_url.expedition_data_params)
-
-    @task(1)
-    def stop(self):
-        profile_url = urls.Profile()
-        self.client.get(profile_url.stop)
-
-    @task(1)
-    def trajectory(self):
-        profile_url = urls.Profile()
-        self.client.get(profile_url.trajectory)
+        login(self)
 
 
 class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
+    task_set = MasterUserBehavior
     min_wait = 1000 * 5
     max_wait = 1000 * 10
--- urls.py
+++ urls.py
@@ -34,6 +34,31 @@ class Login:
         }
 
 
+class ODByRoute:
+    def __init__(self):
+        self.__url_html_prefix = 'odbyroute'
+
+    @property
+    def matrix_url(self):
+        return format_url(['profile', 'odmatrix'])
+
+    @property
+    def matrix_data_url(self):
+        return format_url([URL_API_PREFIX, self.__url_html_prefix, 'matrixData'])
+
+    def get_matrix_data_params(self):
+        start_date = datetime(2017, 4, 4)
+        end_date = start_date + timedelta(days=random.randint(90, 130))
+        auth_route_list = ['T101 00I', 'T505 00I', 'T517 00I']
+        auth_route = auth_route_list[random.randint(0, len(auth_route_list) - 1)]
+
+        return {
+            'startDate': str(start_date),
+            'endDate': str(end_date),
+            'authRoute': auth_route
+        }
+
+
 class Profile:
     def __init__(self):
         self.__url_html_prefix = 'profile'
@@ -47,30 +72,32 @@ class Profile:
     """
 
     @property
-    def expedition(self):
+    def expedition_url(self):
         return format_url([self.__url_html_prefix, 'expedition'])
 
     @property
-    def stop(self):
+    def stop_url(self):
         return format_url([self.__url_html_prefix, 'stop'])
 
     @property
-    def trajectory(self):
+    def trajectory_url(self):
         return format_url([self.__url_html_prefix, 'trajectory'])
 
     @property
     def expedition_data_url(self):
-        params = '?{0}'.format()
-        return format_url([URL_API_PREFIX, self.__url_html_prefix, 'loadProfileByExpeditionData' + params])
+        return format_url([URL_API_PREFIX, self.__url_html_prefix, 'loadProfileByExpeditionData'])
+
+    @property
+    def stop_data_url(self):
+        return format_url([URL_API_PREFIX, self.__url_html_prefix, 'loadProfileByStopData'])
 
     """
     DATA CALLS
     """
 
-    @property
-    def expedition_data_params(self):
+    def get_expedition_data_params(self):
         start_date = datetime(2017, 5, 8)
-        end_date = start_date + timedelta(days=random.randint(0, 6))
+        end_date = start_date + timedelta(days=random.randint(30, 120))
         auth_route_list = ['T101 00I', 'T505 00I', 'T517 00I']
         auth_route = auth_route_list[random.randint(0, len(auth_route_list) - 1)]
 
@@ -78,4 +105,91 @@ class Profile:
             'startDate': str(start_date),
             'endDate': str(end_date),
             'authRoute': auth_route
+        }
+
+    def get_stop_data_params(self):
+        start_date = datetime(2017, 5, 8)
+        end_date = start_date + timedelta(days=random.randint(30, 120))
+        stop_code_list = ['PA433', 'PI61', 'PA347']
+        stop_code = stop_code_list[random.randint(0, len(stop_code_list) - 1)]
+
+        return {
+            'startDate': str(start_date),
+            'endDate': str(end_date),
+            'stopCode': stop_code
+        }
+
+
+class Speed:
+
+    def __init__(self):
+        self.__url_html_prefix = 'speed'
+
+    @property
+    def index(self):
+        return format_url([self.__url_html_prefix])
+
+    """
+    HTML CALLS
+    """
+
+    @property
+    def matrix_url(self):
+        return format_url([self.__url_html_prefix, 'matrix'])
+
+    @property
+    def ranking_url(self):
+        return format_url([self.__url_html_prefix, 'ranking'])
+
+    @property
+    def variation_url(self):
+        return format_url([self.__url_html_prefix, 'variation'])
+
+    @property
+    def matrix_data_url(self):
+        return format_url([URL_API_PREFIX, self.__url_html_prefix, 'matrixData'])
+
+    @property
+    def ranking_data_url(self):
+        return format_url([URL_API_PREFIX, self.__url_html_prefix, 'rankingData'])
+
+    @property
+    def variation_data_url(self):
+        return format_url([URL_API_PREFIX, self.__url_html_prefix, 'speedVariation'])
+
+    """
+    DATA CALLS
+    """
+
+    def get_matrix_data_params(self):
+        start_date = datetime(2017, 3, 1)
+        end_date = start_date + timedelta(days=random.randint(200, 300))
+        auth_route_list = ['T101 00I', 'T505 00I', 'T517 00I']
+        auth_route = auth_route_list[random.randint(0, len(auth_route_list) - 1)]
+
+        return {
+            'startDate': str(start_date),
+            'endDate': str(end_date),
+            'authRoute': auth_route
+        }
+
+    def get_ranking_data_params(self):
+        start_date = datetime(2017, 3, 1)
+        end_date = start_date + timedelta(days=random.randint(200, 300))
+
+        return {
+            'startDate': str(start_date),
+            'endDate': str(end_date),
+            'hourPeriodFrom': 0,
+            'hourPeriodTo': 23
+        }
+
+    def get_variation_data_params(self):
+        base_date = datetime(2017, 3, 1)
+        start_date = base_date + timedelta(days=random.randint(200, 300))
+        operator = random.randint(1, 7)
+
+        return {
+            'startDate': str(start_date),
+            'operator': operator,
         }
