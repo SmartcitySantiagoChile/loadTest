dde5ca0d43774481ffec2d8fa3afa364793504bc
Felipe Hernández <cephei.1313@gmail.com>
2018-05-08 11:01:48 -0300

    fixed method name

--- task/trip.py
+++ task/trip.py
@@ -14,35 +14,35 @@ class TripUserBehavior(TaskSet):
     def resume(self):
         self.client.get(self.trip_url.resume_url)
         self.client.get(self.trip_url.available_days_url)
-        self.client.get(self.trip_url.resume_data_url, params=self.trip_url.get_resume_params(),
+        self.client.get(self.trip_url.resume_data_url, params=self.trip_url.get_resume_data_params(),
                         name=self.trip_url.resume_data_url)
 
     @task(10)
     def map(self):
         self.client.get(self.trip_url.map_url)
         self.client.get(self.trip_url.available_days_url)
-        self.client.get(self.trip_url.map_data_url, params=self.trip_url.get_map_params(),
+        self.client.get(self.trip_url.map_data_url, params=self.trip_url.get_map_data_params(),
                         name=self.trip_url.map_data_url)
 
     @task(10)
     def large_trips(self):
         self.client.get(self.trip_url.large_trips_url)
         self.client.get(self.trip_url.available_days_url)
-        self.client.get(self.trip_url.large_trips_data_url, params=self.trip_url.get_large_trips_params(),
+        self.client.get(self.trip_url.large_trips_data_url, params=self.trip_url.get_large_trips_data_params(),
                         name=self.trip_url.large_trips_data_url)
 
     @task(10)
     def from_to_maps(self):
         self.client.get(self.trip_url.from_to_map_url)
         self.client.get(self.trip_url.available_days_url)
-        self.client.get(self.trip_url.from_to_map_data_url, params=self.trip_url.get_from_to_map_params(),
+        self.client.get(self.trip_url.from_to_map_data_url, params=self.trip_url.get_from_to_map_data_params(),
                         name=self.trip_url.from_to_map_data_url)
 
     @task(10)
     def strategies(self):
         self.client.get(self.trip_url.strategies_url)
         self.client.get(self.trip_url.available_days_url)
-        self.client.get(self.trip_url.strategies_data_url, params=self.trip_url.get_strategies_params(),
+        self.client.get(self.trip_url.strategies_data_url, params=self.trip_url.get_strategies_data_params(),
                         name=self.trip_url.strategies_data_url)
 
     @task(50)
